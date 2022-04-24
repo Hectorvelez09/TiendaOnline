db3cd3c4c75e96676bc7999245f00cf01c43e779
El Negro <El Negro@HECTOR09>
2022-04-24 14:42:11 -0500

    Improve countries CRUD

---
+++ TiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Web.Models;
+
+namespace TiendaOnline.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //Este es un contructor que se deja vacio
+        }
+        public DbSet<Country> countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
